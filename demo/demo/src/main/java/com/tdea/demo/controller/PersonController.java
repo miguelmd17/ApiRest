5ca3133cb46f49fcea79a32d67830676770fbbb5
miguelmd17 <miguelmontenegro017@gmail.com>
2025-09-22 01:17:09 -0500

    Creacion ApiRest

---
+++ demo/demo/src/main/java/com/tdea/demo/controller/PersonController.java
@@ -0,0 +1,49 @@
+package com.tdea.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tdea.demo.modelo.repository.PersonRepository;
+import com.tdea.demo.modelo.Person;
+
+@RestController
+@RequestMapping("/api")
+public class PersonController {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @GetMapping("/persons")
+    public List<Person> allPersons() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/person/{name}")
+    public List<Person> findByName(@PathVariable("name") String name) {
+        return repository.findByName(name);
+    }
+
+    @PostMapping("/person")
+    public Person createPerson(@RequestBody Person person) {
+        return repository.save(person);
+    }
+
+    @PutMapping("/person/{id}")
+    public Person updatePerson(@PathVariable int id, @RequestBody Person person) {
+        return repository.save(person);
+    }
+
+    @DeleteMapping("/person/{id}")
+    public void deletePerson(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+    }
+}
